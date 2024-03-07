75b47dab2bb5e5a0b4bbb67cdc86fa4677a36051
Mohammadwajahat123 <161840548+Mohammadwajahat123@users.noreply.github.com>
2024-03-07 07:11:14 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+let personName = "Eric";
+console.log(` HELLO ${personName}, Would you like to learn some python course with me`);
+var wajahat = "Hussain";
+console.log(`HELLO ${wajahat}, Soon we will meet again bro`);
+const txt = "HI";
+console.log(`HELLO ${txt}, What is going on this evening`);
---
+++ main.ts
@@ -0,0 +1,7 @@
+let personName : string ="Eric"
+console.log(` HELLO ${personName}, Would you like to learn some python course with me`);
+
+var wajahat : string ="Hussain"
+console.log(`HELLO ${wajahat}, Soon we will meet again bro`);
+const txt : string ="HI"
+console.log(`HELLO ${txt}, What is going on this evening`);
